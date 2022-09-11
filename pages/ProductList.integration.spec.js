29329fe30139e5aa42acf802c08a7972c0251944
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2022-09-11 11:44:29 -0300

    test: :white_check_mark: add product list search integration test

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -60,6 +60,39 @@ describe('ProductList - integration', () => {
     expect(cards).toHaveLength(10);
   });
 
+  it('should filter the product list when a search is performed', async () => {
+    // Arrange
+    const products = [
+      ...server.createList('product', 10),
+      server.create('product', {
+        title: 'Meu relógio amado',
+      }),
+      server.create('product', {
+        title: 'Meu outro relógio estimado',
+      }),
+    ];
+
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await nextTick();
+
+    // Act
+    const search = wrapper.findComponent(Search);
+    search.find('input[type="search"]').setValue('relógio');
+    await search.find('form').trigger('submit');
+
+    // Assert
+    const cards = wrapper.findAllComponents(ProductCard);
+    expect(wrapper.vm.searchTerm).toEqual('relógio');
+    expect(cards).toHaveLength(2);
+  });
+
   it('should display the erro message when Promise rejects', async () => {
     axios.get.mockReturnValue(Promise.reject(new Error('')));
 
