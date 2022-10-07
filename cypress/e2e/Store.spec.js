1df0f001c16fddb64f35fdfe50158f1d44210f34
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2022-10-06 21:01:42 -0300

    test: :white_check_mark: test search e2e test

--- cypress/e2e/Store.spec.js
+++ cypress/e2e/Store.spec.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-undef */
+// <reference types="cypress" />
+
 import { makeServer } from '../../miragejs/server';
 
 context('Store', () => {
@@ -17,5 +19,31 @@ context('Store', () => {
 
     cy.get('body').contains('Brand');
     cy.get('body').contains('Wrist Watch');
+  });
+
+  context('Store > Search for Products', () => {
+    it('should type in the search field', () => {
+      cy.visit('http://localhost:3000');
+
+      cy.get('input[type="search"]')
+        .type('Some text here')
+        .should('have.value', 'Some text here');
+    });
+
+    it.only('should type in the search field', () => {
+      server.create('product', {
+        title: 'Relógio bonito',
+      });
+
+      server.createList('product', 10);
+
+      cy.visit('http://localhost:3000');
+
+      cy.get('input[type="search"]').type('Relógio bonito');
+
+      cy.get('[data-testid="search-form"]').submit();
+
+      cy.get('[data-testid="product-card"]').should('have.length', 1);
+    });
   });
 });
